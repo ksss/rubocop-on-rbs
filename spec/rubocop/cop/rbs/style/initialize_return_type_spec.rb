60dac4121b2f3b096310155228d6e25c684841eb
ksss <co000ri@gmail.com>
2024-06-12 17:52:58 +0900

    Ignore when return untyped

--- spec/rubocop/cop/rbs/style/initialize_return_type_spec.rb
+++ spec/rubocop/cop/rbs/style/initialize_return_type_spec.rb
@@ -6,14 +6,22 @@ RSpec.describe RuboCop::Cop::RBS::Style::InitializeReturnType, :config do
   it 'registers an offense' do
     expect_offense(<<~RBS)
       class Foo
-        def initialize: () -> untyped
-                              ^^^^^^^ `#initialize` method should return `void`
+        def initialize: () -> nil
+                              ^^^ `#initialize` method should return `void`
       end
     RBS
 
     expect_correction(<<~RBS)
       class Foo
         def initialize: () -> void
+      end
+    RBS
+  end
+
+  it 'not registers an offense' do
+    expect_no_offenses(<<~RBS)
+      class Foo
+        def initialize: () -> untyped
       end
     RBS
   end
