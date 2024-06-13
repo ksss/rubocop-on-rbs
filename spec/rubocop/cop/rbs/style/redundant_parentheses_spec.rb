03ee5252d480ae3105526123354b4d94f01f0de4
ksss <co000ri@gmail.com>
2024-06-13 21:37:27 +0900

    Add RBS/Style/RedundantParentheses

---
+++ spec/rubocop/cop/rbs/style/redundant_parentheses_spec.rb
@@ -0,0 +1,43 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+RSpec.describe RuboCop::Cop::RBS::Style::RedundantParentheses, :config do
+  it 'registers an offense' do
+    expect_offense(<<~RBS)
+      class Foo
+        def foo: ( ( Integer ) | ( String ) ) -> void
+                   ^^^^^^^^^^^ Don't use parentheses around simple type.
+                                 ^^^^^^^^^^ Don't use parentheses around simple type.
+        def bar: () -> (Integer)
+                       ^^^^^^^^^ Don't use parentheses around simple type.
+        def proc: (^((bool)) -> void) -> (void)
+                     ^^^^^^ Don't use parentheses around simple type.
+                                         ^^^^^^ Don't use parentheses around simple type.
+        def optional: () -> ((1 | 2)?)
+                            ^^^^^^^^^^ Don't use parentheses around simple type.
+      end
+    RBS
+
+    expect_correction(<<~RBS)
+      class Foo
+        def foo: (  Integer  |  String  ) -> void
+        def bar: () -> Integer
+        def proc: (^(bool) -> void) -> void
+        def optional: () -> (1 | 2)?
+      end
+    RBS
+  end
+
+  it 'does not register an offense' do
+    expect_no_offenses(<<~RBS)
+      class Foo
+        def foo: () -> (1 | 2)
+        def bar: ((1 | 2)) -> void
+        def block: () { (bool) -> void } -> void
+        def proc: (^(bool) -> void) -> void
+        def param: [T] (^(T) { (T) -> void } -> T, ^(T) -> T) { (T) -> T } -> (^(T) -> T)
+      end
+    RBS
+  end
+end
