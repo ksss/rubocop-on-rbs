db6287a88ddef93bc26805b50950213ee1310773
ksss <co000ri@gmail.com>
2024-11-17 22:55:41 +0900

    Add RBS/Lint/TopLevelTypeAlias

---
+++ spec/rubocop/cop/rbs/lint/top_level_type_alias_spec.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+RSpec.describe RuboCop::Cop::RBS::Lint::TopLevelTypeAlias, :config do
+  it 'should registers an offense' do
+    expect_offense(<<~RBS)
+      type foo = String
+      ^^^^^^^^^^^^^^^^^ Top level type alias detected.
+
+      class Foo
+        type bar = Integer
+      end
+
+      type baz = Integer
+      ^^^^^^^^^^^^^^^^^^ Top level type alias detected.
+    RBS
+  end
+end
