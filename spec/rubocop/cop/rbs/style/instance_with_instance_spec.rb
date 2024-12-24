3a79e535a7cc88ba196852f821b48dcf1aa334d8
ksss <co000ri@gmail.com>
2024-12-24 19:03:44 +0900

    Reimplement RBS/Style/InstanceWithInstance
    - Module is not target since it has a different meaning from `instance` and `self`.
    - Interface has `interface` as invalid.
    - The meaning of `self` and `instance` changes in generic class.

--- spec/rubocop/cop/rbs/style/instance_with_instance_spec.rb
+++ spec/rubocop/cop/rbs/style/instance_with_instance_spec.rb
@@ -6,8 +6,12 @@ RSpec.describe RuboCop::Cop::RBS::Style::InstanceWithInstance, :config do
   it 'registers an offense' do
     expect_offense(<<~RBS)
       class Foo
-        def foo: () -> instance
-                       ^^^^^^^^ Use `self` instead of `instance`.
+        def foo: (instance) -> instance
+                  ^^^^^^^^ Use `self` instead of `instance`.
+                               ^^^^^^^^ Use `self` instead of `instance`.
+
+        def bar: [T] () { () [self: instance] -> void } -> T
+                                    ^^^^^^^^ Use `self` instead of `instance`.
 
         def self.foo: () -> instance
 
@@ -24,7 +28,9 @@ RSpec.describe RuboCop::Cop::RBS::Style::InstanceWithInstance, :config do
 
     expect_correction(<<~RBS)
       class Foo
-        def foo: () -> self
+        def foo: (self) -> self
+
+        def bar: [T] () { () [self: self] -> void } -> T
 
         def self.foo: () -> instance
 
@@ -35,6 +41,50 @@ RSpec.describe RuboCop::Cop::RBS::Style::InstanceWithInstance, :config do
         @@cvar: instance
 
         self.@civar: instance
+      end
+    RBS
+  end
+
+  it 'registers an offense on inherited generic class' do
+    expect_offense(<<~RBS)
+      class Bar < Foo[Integer]
+        def foo: (instance) -> instance
+                  ^^^^^^^^ Use `self` instead of `instance`.
+                               ^^^^^^^^ Use `self` instead of `instance`.
+      end
+    RBS
+
+    expect_correction(<<~RBS)
+      class Bar < Foo[Integer]
+        def foo: (self) -> self
+      end
+    RBS
+  end
+
+  it 'does not register an offense' do
+    expect_no_offenses(<<~RBS)
+      class Generic[T]
+        def a: () -> instance
+      end
+    RBS
+
+    expect_no_offenses(<<~RBS)
+      module Foo
+        def foo: () -> instance
+
+        @ivar: instance
+      end
+
+      class Bar
+        module Foo
+          def foo: () -> instance
+        end
+      end
+    RBS
+
+    expect_no_offenses(<<~RBS)
+      interface _Foo
+        def foo: () -> instance
       end
     RBS
   end
