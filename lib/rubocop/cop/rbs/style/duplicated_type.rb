534acd93a608219992ea585961912b0945bf14c1
ksss <co000ri@gmail.com>
2025-04-17 13:37:40 +0900

    [RBS/Style/DuplicatedType] More support decls

--- lib/rubocop/cop/rbs/style/duplicated_type.rb
+++ lib/rubocop/cop/rbs/style/duplicated_type.rb
@@ -25,6 +25,12 @@ module RuboCop
             end
           end
 
+          def on_rbs_constant(decl) = check_type(decl.type)
+          alias on_rbs_global on_rbs_constant
+          alias on_rbs_type_alias on_rbs_constant
+          alias on_rbs_attribute on_rbs_constant
+          alias on_rbs_var on_rbs_constant
+
           # @rbs type: ::RBS::Types::t
           def check_type(type)
             case type
