378077a1ebc23d45a3190abb9b8cdd4bff0050a1
ksss <co000ri@gmail.com>
2025-05-16 09:51:20 +0900

    Add RBS/Layout/EmptyLineBetweenDeclarations

--- lib/rubocop/cop/rbs_cops.rb
+++ lib/rubocop/cop/rbs_cops.rb
@@ -3,6 +3,7 @@
 require 'rbs'
 
 require_relative 'rbs/layout/comment_indentation'
+require_relative 'rbs/layout/empty_line_between_declarations'
 require_relative 'rbs/layout/empty_lines_around_access_modifier'
 require_relative 'rbs/layout/empty_lines_around_body'
 require_relative 'rbs/layout/empty_lines_around_class_body'
